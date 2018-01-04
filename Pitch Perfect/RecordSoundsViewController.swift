f87d0a5ee56e70d8458dffb31b79ab1890be5fd6
Hao Wu <hao.wu.au@gmail.com>
2018-01-04 23:13:24 +1100

    App is able to work.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -37,11 +37,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showPlaySounds" {
             let playSoundsVC = segue.destination as! PlaySoundsViewController
